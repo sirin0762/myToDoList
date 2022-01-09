8752b5a6e7edc8c8477556d95671de986bb7c5f5
sirin0762 <tlsdydwls123@gmail.com>
2022-01-09 17:36:08 +0900

    REFACTOR : 컨트롤러에서 세션 불러오는 방식을 @LoginUser 어노테이션 방식으로 변경

--- src/main/java/com/yongjin/springboot/controller/IndexController.java
+++ src/main/java/com/yongjin/springboot/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.yongjin.springboot.controller;
 
 import com.sun.org.apache.xpath.internal.operations.Mod;
+import com.yongjin.springboot.config.auth.LoginUser;
 import com.yongjin.springboot.config.auth.dto.SessionUser;
 import com.yongjin.springboot.controller.dto.PostsResponseDto;
 import com.yongjin.springboot.service.posts.PostsService;
@@ -18,18 +19,13 @@ public class IndexController {
 
     private final PostsService postsService;
 
-    private final HttpSession httpSession;
-
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
-
         return "index";
     }
 
